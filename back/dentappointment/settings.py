846b0a3689544b6db4da11179228612292e18f95
Mizael V <misael_v_g@outlook.com>
2024-06-04 00:24:01 -0600

    [Update] confg added for media files and static files

--- back/dentappointment/settings.py
+++ back/dentappointment/settings.py
@@ -151,17 +151,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 # Define the directory where static files will be collected
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Ensure the directory exists
 if not os.path.exists(STATIC_ROOT):
