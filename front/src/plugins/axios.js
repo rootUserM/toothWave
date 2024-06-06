cd81771e89fb2ee4b9c1df717fb6df116cf7fc62
Mizael V <misael_v_g@outlook.com>
2024-06-06 06:55:55 -0600

    [Update] axios config

--- front/src/plugins/axios.js
+++ front/src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "@/router";
 import store from "@/store";
 const instance = axios.create({
-  baseURL: "http://68.183.140.197/api",
+  baseURL: "http://citassalud.com.mx/api",
   timeout: 5000,
 });
 
